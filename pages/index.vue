efc4290fabcac2f732f2a824d453a724a0e931c4
Nathan <njgingrich@gmail.com>
2017-12-22 16:58:37 -0500

    base install from nuxt removing extra crud

---
+++ pages/index.vue
@@ -0,0 +1,18 @@
+<template>
+  <section class="container">
+    <div>
+      <logo/>
+      <h2 class="subtitle">
+        What is Congress doing today?
+      </h2>
+    </div>
+  </section>
+</template>
+
+<script>
+export default {
+}
+</script>
+
+<style lang="scss">
+</style>
