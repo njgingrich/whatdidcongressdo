37a5605a203649a3bbf95eae9651b49abb3ac709
Nathan <njgingrich@gmail.com>
2017-12-24 01:16:15 -0500

    update grid layout for better reuse

---
+++ components/Foot.vue
@@ -0,0 +1,29 @@
+<template>
+  <footer class="footer">
+    <p>Copyright 2017 Nathan Gingrich - <a href="https://twitter.com/njgingrich" target="_blank" rel="noopener">@njgingrich</a></p>
+  </footer>
+</template>
+<script>
+export default {
+
+}
+</script>
+<style lang="scss" scoped>
+@import "~assets/styles/colors";
+
+.footer {
+  display: flex;
+  align-items: flex-end;
+  justify-content: center;
+  padding-bottom: 16px;
+
+  background-color: $blue;
+  color: $white;
+  font-weight: bold;
+
+  p a, p a:visited, p a:active {
+    color: $twitter-color;
+    text-decoration: none;
+  }
+}
+</style>
--- layouts/default.vue
+++ layouts/default.vue
@@ -1,10 +1,19 @@
 <template>
-  <div>
+  <div class="container">
+    <Navbar/>
     <nuxt/>
+    <Foot/>
   </div>
 </template>
 <script>
+import Foot from '~/components/Foot'
+import Navbar from '~/components/Navbar'
+
 export default {
+  components: {
+    Foot,
+    Navbar
+  }
 }
 </script>
 
@@ -12,7 +21,7 @@ export default {
 @import "~assets/styles/colors";
 
 html {
-  font-family: "Source Sans Pro", -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, "Helvetica Neue", Arial, sans-serif;
+  font-family: "Merriweather", -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, "Helvetica Neue", Arial, sans-serif;
   font-size: 16px;
   word-spacing: 1px;
   -ms-text-size-adjust: 100%;
@@ -40,5 +49,27 @@ a.link-item, a.link-item:visited, a.link-item:active {
   font-family: 'EB Garamond';
   font-weight: bold;
   text-transform: uppercase;
+}
+
+.container {
+  display: grid;
+  grid-template-columns: 1fr;
+  grid-template-rows: 56px auto minmax(120px, auto);
+  grid-template-areas:
+    "navbar"
+    "page"
+    "footer";
+}
+
+.top-navbar {
+  grid-area: navbar;
+}
+
+.footer {
+  grid-area: footer;
+}
+
+.page {
+  grid-area: page;
 }
 </style>
