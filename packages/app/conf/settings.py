87253688023d1ef713d93c3fa89e03c914ba8590
Nathan Gingrich <njgingrich@gmail.com>
2024-11-17 18:01:25 -0600

    Add index page and tweak some css

--- packages/app/conf/settings.py
+++ packages/app/conf/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -37,7 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'congress_api'
+    'congress_api',
+    'ui'
 ]
 
 MIDDLEWARE = [
@@ -117,6 +119,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT= os.path.join(BASE_DIR,'static')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
--- packages/app/conf/urls.py
+++ packages/app/conf/urls.py
@@ -15,8 +15,14 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
+from django.urls import include
 from django.urls import path
+from django.conf import settings
+from django.conf.urls.static import static
+
+from ui import urls
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-]
+    path('', include(urls)),
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
